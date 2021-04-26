c1e79cdeb1ea4fba4e11b5380ac11d3642a1f3b5
tif-calin <culitif@tuta.io>
2021-04-26 16:47:31 -0700

    set up tests

---
+++ src/array-map.js
@@ -0,0 +1,23 @@
+export function doubleNumbers(arr) {
+    return arr.map(n => n * 2);
+}
+
+export function stringItUp(arr){
+  
+}
+
+export function capitalizeNames(arr){
+  
+}
+
+export function namesOnly(arr){
+  
+}
+
+export function makeStrings(arr){
+  
+}
+
+export function readyToPutInTheDOM(arr){
+  
+}
---
+++ src/array-map.test.js
@@ -0,0 +1,7 @@
+import { doubleNumbers } from './array-map';
+
+test('doubles numbers', () => {
+  const input = [2, 5, 100]; // arrange
+  const output = doubleNumbers(input); // act
+  expect(output).toEqual([4, 10, 200]); // assert
+});

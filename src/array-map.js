0849ac5f2ca36cdc4b26ae093d610c768e7d040e
tif-calin <culitif@tuta.io>
2021-04-26 17:02:09 -0700

    implement functions

--- src/array-map.js
+++ src/array-map.js
@@ -2,22 +2,22 @@ export function doubleNumbers(arr) {
     return arr.map(n => n * 2);
 }
 
-export function stringItUp(arr){
-  
+export function stringItUp(arr) {
+  return arr.map(n => n.toString());
 }
 
-export function capitalizeNames(arr){
-  
+export function capitalizeNames(arr) {
+  return arr.map(name => name[0].toUpperCase() + name.slice(1).toLowerCase());
 }
 
-export function namesOnly(arr){
-  
+export function namesOnly(arr) {
+  return arr.map(obj => obj.name);
 }
 
-export function makeStrings(arr){
-  
+export function makeStrings(arr) {
+  return arr.map(obj => `${obj.name} ${obj.age < 50 ? 'is under age!!' : 'can go to the Matrix'}`)
 }
 
-export function readyToPutInTheDOM(arr){
-  
+export function readyToPutInTheDOM(arr) {
+    return arr.map(obj => `<h1>${obj.name}</h1><h2>${obj.age}</h2>`)  
 }

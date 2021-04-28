16b53f5f7bc8179ad6274bf718d2f18fd4ca5226
tif-calin <culitif@tuta.io>
2021-04-28 15:02:05 -0700

    implement sortedArraysByValuesLength

--- src/object-keys-entries.js
+++ src/object-keys-entries.js
@@ -12,5 +12,5 @@ export function getArrayOfKeysAndValues(obj){
 }
 
 export function sortedArraysByValuesLength(obj) {
-
+  return Object.entries(obj).filter((a, b) => a[1].length - b[1].length);
 }

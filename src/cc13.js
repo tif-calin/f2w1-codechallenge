bb4652c686c3c8267f084bc0403da7e61cd470a8
tif-calin <culitif@tuta.io>
2021-05-13 12:00:33 -0700

    containsW

--- src/cc13.js
+++ src/cc13.js
@@ -1,3 +1,8 @@
 export const sortByChildren = (charArray) => {
   return charArray.sort((a, b) => (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0).sort((a, b) => a.children.length - b.children.length);
 };
+
+export const containsW = str => {
+  const re = new RegExp('w');
+  return re.test(str);
+};
--- src/cc13.test.js
+++ src/cc13.test.js
@@ -1,6 +1,6 @@
 // imports
 import { describe, expect, test } from '@jest/globals';
-import { sortByChildren } from './cc13.js';
+import { containsW, sortByChildren } from './cc13.js';
 
 // data
 let characters = [
@@ -99,5 +99,28 @@ describe('challenge 1: sortByChildren', () => {
 
     expect(sortByChildren(input)).toStrictEqual(expected);
     expect(sortByChildren(input)[0].children.length).toStrictEqual(0);
+  });
+});
+
+describe('challenge 2: containsW', () => {
+  test('does "hello world" contain "w"?', () => {
+    const input = 'hello world';
+    const expected = true;
+
+    expect(containsW(input)).toStrictEqual(expected);
+  });
+
+  test('does "Hello World" contain "w"?', () => {
+    const input = 'Hello World';
+    const expected = false;
+
+    expect(containsW(input)).toStrictEqual(expected);
+  });
+
+  test('does "hello everyone" contain "w"?', () => {
+    const input = 'hello everyone';
+    const expected = false;
+
+    expect(containsW(input)).toStrictEqual(expected);
   });
 });
